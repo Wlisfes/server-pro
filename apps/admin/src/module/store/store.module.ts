c8ac01c77af3a9da66f687def92c90c9dac10da9
情雨随风 <876451336@qq.com>
2020-04-27 21:02:19 +0800

    因项目问题需要把整个项目初始化

--- apps/admin/src/module/store/store.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { StoreService } from './store.service'
-
-@Module({
-	providers: [StoreService],
-	exports: [StoreService]
-})
-export class StoreModule {}
