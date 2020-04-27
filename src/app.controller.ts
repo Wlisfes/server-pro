b240cb0f2f0910a58b0d4ed748a1140a31f3c476
情雨随风 <876451336@qq.com>
2020-04-27 21:44:34 +0800

    增加图片上传阿里云OSS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, UseInterceptors, UploadedFile } from '@nestjs/common'
+import { AppService } from './app.service'
+import { FileInterceptor } from '@nestjs/platform-express'
+import { ApiTags, ApiOperation } from '@nestjs/swagger'
 
 @Controller()
+@ApiTags('根模块')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get()
+	getHello(): string {
+		return this.appService.getHello()
+	}
+
+	@Post('api/upload')
+	@ApiOperation({ summary: '图片上传' })
+	@UseInterceptors(FileInterceptor('file'))
+	async upload(@UploadedFile('file') file) {
+		return file
+	}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,16 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+
+/**OSS模块*******************************/
+import { MulterModule } from '@nestjs/platform-express'
+import * as MAO from 'multer-aliyun-oss'
+
+/**守卫*******************************/
+import { APP_GUARD } from '@nestjs/core'
+import { AuthGuard } from './guard/auth.guard'
+
+/**逻辑模块*******************************/
 import { CommonModule } from './module/common/common.module'
 import { UserModule } from './module/user/user.module'
 import { RoleModule } from './module/role/role.module'
@@ -9,8 +19,35 @@ import { StoreModule } from './module/store/store.module'
 import { AuthModule } from './module/auth/auth.module'
 
 @Module({
-	imports: [CommonModule, UserModule, RoleModule, AuthModule, SignModule, StoreModule],
+	imports: [
+		CommonModule,
+		UserModule,
+		RoleModule,
+		AuthModule,
+		SignModule,
+		StoreModule,
+		MulterModule.registerAsync({
+			useFactory() {
+				return {
+					storage: MAO({
+						config: {
+							region: process.env.OSS_REGION,
+							accessKeyId: process.env.OSS_ACCESS_KEY_ID,
+							accessKeySecret: process.env.OSS_ACCESS_KEY_SECRET,
+							bucket: process.env.OSS_BUCKET
+						}
+					})
+				}
+			}
+		})
+	],
 	controllers: [AppController],
-	providers: [AppService]
+	providers: [
+		AppService,
+		{
+			provide: APP_GUARD,
+			useClass: AuthGuard
+		}
+	]
 })
 export class AppModule {}
