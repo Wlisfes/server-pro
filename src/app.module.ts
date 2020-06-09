8bbed5d39394fccc9ef9ec7ad5fd2d5bda039bd7
情雨随风 <876451336@qq.com>
2020-06-09 09:37:45 +0800

    新增后台模块主入口

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,29 +2,12 @@ import { Module } from '@nestjs/common'
 import { AppController } from '@/app.controller'
 import { AppService } from '@/app.service'
 
-//守卫
-import { APP_GUARD } from '@nestjs/core'
-import { AuthGuard } from '@/guard/auth.guard'
-
-//模块
-import { CommonModule } from '@/module/admin/common/common.module'
-import { UserModule } from '@/module/admin/user/user.module'
-import { RoleModule } from '@/module/admin/role/role.module'
-import { AuthModule } from '@/module/admin/auth/auth.module'
-import { StoreModule } from '@/module/admin/store/store.module'
-import { TagModule } from '@/module/admin/tag/tag.module'
-import { ArticleModule } from '@/module/admin/article/article.module'
-import { ProjectModule } from '@/module/admin/project/project.module'
+//各端主模块入口导入
+import { AdminModule } from '@/module/admin/admin/admin.module'
 
 @Module({
-	imports: [CommonModule, UserModule, RoleModule, AuthModule, StoreModule, TagModule, ArticleModule, ProjectModule],
+	imports: [AdminModule],
 	controllers: [AppController],
-	providers: [
-		AppService,
-		{
-			provide: APP_GUARD,
-			useClass: AuthGuard
-		}
-	]
+	providers: [AppService]
 })
 export class AppModule {}
---
+++ src/module/admin/admin/admin.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common'
+
+//守卫
+import { APP_GUARD } from '@nestjs/core'
+import { AuthGuard } from '@/guard/auth.guard'
+
+//模块
+import { CommonModule } from '@/module/admin/common/common.module'
+import { UserModule } from '@/module/admin/user/user.module'
+import { RoleModule } from '@/module/admin/role/role.module'
+import { AuthModule } from '@/module/admin/auth/auth.module'
+import { StoreModule } from '@/module/admin/store/store.module'
+import { TagModule } from '@/module/admin/tag/tag.module'
+import { ArticleModule } from '@/module/admin/article/article.module'
+import { ProjectModule } from '@/module/admin/project/project.module'
+
+@Module({
+	imports: [CommonModule, UserModule, RoleModule, AuthModule, StoreModule, TagModule, ArticleModule, ProjectModule],
+	providers: [
+		{
+			provide: APP_GUARD,
+			useClass: AuthGuard
+		}
+	],
+	exports: [UserModule, RoleModule, AuthModule, StoreModule, TagModule, ArticleModule, ProjectModule]
+})
+export class AdminModule {}
